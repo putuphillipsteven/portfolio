73880d43bf6a31401145684c281cb35f7cf0fb9e
phillip-steven <putu.phillip@gmail.com>
2024-03-20 16:37:58 +0700

    fix(home)
    adding grid display to homepage

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,21 +3,25 @@ import { Home } from './pages/home/container';
 
 export const App = () => {
 	return (
-		<div className='p-4 font-mono bg-gray-200 max-w-screen h-screen'>
-			<div className='p-2 border border-black max-w-full min-h-full flex flex-col gap-y-2'>
-				<div className='p-2 bg-red-200'>
-					<h2 className='text-2xl font-bold'>I Putu Phillip Steven</h2>
+		<div className='p-4 font-mono  max-w-screen h-screen'>
+			<div className='p-2 border-2 border-black  max-w-full min-h-full grid gap-y-2 grid-rows-[12%_auto] grid-cols-1 content-stretch bg-gray-100 '>
+				<div className='p-2 max-h-full flex flex-col justify-between overflow-hidden'>
+					<h2 className='text-3xl font-medium'>I Putu Phillip Steven</h2>
 					<p>Web Developer</p>
 				</div>
-				<div className='p-2 bg-red-200 max-w-full min-h-full flex justify-between gap-x-2'>
-					<div className='bg-green-200 w-4/12'>
+				<div className='p-2 flex justify-between gap-x-2'>
+					<div className=' w-4/12'>
 						<ul>
 							<li>Home</li>
+							<li>About</li>
+							<li>Skills</li>
+							<li>Project</li>
+							<li>Contact Me</li>
 						</ul>
 					</div>
-					<div className='bg-green-200 w-8/12'>
+					<div className=' w-8/12'>
 						<ul>
-							<li>Home</li>
+							<li>Content</li>
 						</ul>
 					</div>
 				</div>
