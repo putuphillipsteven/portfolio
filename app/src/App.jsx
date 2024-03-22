bc0dc8a9979aa3c63dfc3434317ddc33b4f5e268
phillip-steven <putu.phillip@gmail.com>
2024-03-22 15:03:44 +0700

    fix(home display)
    make home(about-me) is hidden scrollable

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -28,17 +28,17 @@ export const App = () => {
 	});
 
 	return (
-		<div className='p-8  max-w-screen h-screen'>
-			<div className='p-2 shadow-lg  max-w-full min-h-full grid gap-y-2 grid-rows-[min-content_auto] grid-cols-1 content-stretch bg-gradient-to-b from-gray-200 to-gray-100 rounded-md'>
-				<div className='p-2 max-h-full flex flex-col justify-between overflow-hidden h-fit'>
+		<div className='p-8 max-w-screen h-screen overflow-hidden'>
+			<div className='p-2 shadow-lg  max-w-full h-full grid gap-y-2 grid-rows-[min-content_auto] grid-cols-1 content-stretch bg-gradient-to-b from-gray-200 to-gray-100 rounded-md overflow-hidden'>
+				<div className='p-2 flex flex-col justify-between overflow-hidden h-fit'>
 					<h2 className='text-3xl font-bold'>I Putu Phillip Steven</h2>
 					<p className='text-sm font-semibold'>Web Developer</p>
 				</div>
-				<div className='p-2 flex justify-between gap-x-2'>
+				<div className='p-2 flex justify-between  gap-x-2'>
 					<div className='w-3/12 '>
 						<ul className='flex flex-col gap-y-4'>{linkedListElement}</ul>
 					</div>
-					<div className='w-9/12'>
+					<div className='w-9/12 h-full'>
 						<Outlet />
 					</div>
 				</div>
--- app/src/pages/home/container/index.jsx
+++ app/src/pages/home/container/index.jsx
@@ -1,18 +1,18 @@
-import { Fragment } from 'react';
-
 export const Home = () => {
 	return (
-		<div className=' flex flex-row-reverse h-full'>
-			<p className='text-justify w-9/12 font-semibold self-end'>
-				I am a graduate in automation engineering from State Polytechnic of Bali, graduated in
-				November 2021. Interested in pursuing and looking for opportunities in web development role.
-				Certified in front and back end development. Skilled in HTML, CSS, Javascript, Node, React,
-				and MySql. Recently took a Full Stack Development bootcamp in Purwadhika Digital School. In
-				this bootcamp, I create simple Event Ticket Management, Point of Sales Application, and
-				Multi Warehouse E-Commerce Web App, all is done in groups and using MERN tech stack.
-				Interested in finding a better problem solving for challenging tasks, and learning new
-				technologies and tools.
-			</p>
+		<div className='flex flex-row-reverse h-full b overflow-hidden '>
+			<div className='self-end w-8/12 max-h-[20em] overflow-y-auto no-scrollbar'>
+				<p className='text-justify text-xs sm:text-lg'>
+					I am a graduate in automation engineering from State Polytechnic of Bali, graduated in
+					November 2021. Interested in pursuing and looking for opportunities in web development
+					role. Certified in front and back end development. Skilled in HTML, CSS, Javascript, Node,
+					React, and MySql. Recently took a Full Stack Development bootcamp in Purwadhika Digital
+					School. In this bootcamp, I create simple Event Ticket Management, Point of Sales
+					Application, and Multi Warehouse E-Commerce Web App, all is done in groups and using MERN
+					tech stack. Interested in finding a better problem solving for challenging tasks, and
+					learning new technologies and tools.
+				</p>
+			</div>
 		</div>
 	);
 };
