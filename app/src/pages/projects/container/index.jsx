b89f5b633d510f98c35d356250083dd618ad46c0
phillip-steven <putu.phillip@gmail.com>
2024-03-23 10:38:11 +0700

    fix(works-pages)
    add dynamic rendering

--- app/src/pages/projects/container/index.jsx
+++ app/src/pages/projects/container/index.jsx
@@ -1,25 +1,37 @@
 import { Link } from 'react-router-dom';
 import { MdArrowOutward } from 'react-icons/md';
 
+const projects = [
+	{
+		name: 'Calculator',
+		description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis deserunt eveniet
+						sapiente nostrum animi nam expedita doloremque fugiat exercitationem obcaecati?`,
+		link: `https://putuphillipsteven.github.io/calculator/`,
+		imageLink: `https://lh3.googleusercontent.com/d/1QJCZelGz5PDW9UZM58UVEJkglZkjLUnq=w1000?authuser=0`,
+	},
+];
+
 export const Projects = () => {
+	const renderedProjects = projects?.map((project) => {
+		return (
+			<div className='flex flex-col gap-y-2' key={[project?.name]}>
+				<Link to={`${project?.link}`} target='_blank' className='flex items-center gap-x-2 '>
+					<h2 className='text-sm sm:text-lg border-black border-b-2  font-bold '>
+						{project?.name}
+					</h2>
+					<MdArrowOutward />
+				</Link>
+				<p className='text-justify'>{project?.description}</p>
+				<div className='w-full md:w-7/12 aspect-video border-black border-2'>
+					<img src={`${project?.imageLink}`} />
+				</div>
+			</div>
+		);
+	});
 	return (
 		<div className='flex flex-row-reverse h-full b overflow-hidden w-full'>
 			<div className='self-end w-full sm:w-8/12 max-h-[35em]  overflow-y-auto no-scrollbar'>
-				<div className='flex flex-col gap-y-2'>
-					<Link to={`https://google.com`} target='_blank' className='flex items-center gap-x-2 '>
-						<h2 className='text-sm md:text-lg border-black border-b-2 font-bold'>
-							Event Ticket Management
-						</h2>
-						<MdArrowOutward />
-					</Link>
-					<p className='text-justify'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis deserunt eveniet
-						sapiente nostrum animi nam expedita doloremque fugiat exercitationem obcaecati?
-					</p>
-					<div className='w-full md:w-6/12 aspect-video border-black border-2'>
-						<img />
-					</div>
-				</div>
+				{renderedProjects}
 			</div>
 		</div>
 	);
