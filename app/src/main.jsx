784653224e7180f6001abfb1517d4061804ea6a8
phillip-steven <putu.phillip@gmail.com>
2024-03-23 15:13:06 +0700

    testing rename  ReactDOM import

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import {
 	createRoutesFromElements,
 	createBrowserRouter,
 	Route,
 	RouterProvider,
-	BrowserRouter,
 } from 'react-router-dom';
 import { App } from './App';
 import { Home } from './pages/home/container';
