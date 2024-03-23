ec71e98c29067cd783e512c939bc85b5b452c27c
phillip-steven <putu.phillip@gmail.com>
2024-03-23 11:20:42 +0700

    feat(contact)
    add contact-pages

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -12,6 +12,7 @@ import { Home } from './pages/home/container';
 import { ErrorPages } from './pages/error-pages/container';
 import { Skills } from './pages/skills/container';
 import { Works } from './pages/works/container';
+import { Contact } from './pages/contact/container';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -20,6 +21,7 @@ const router = createBrowserRouter(
 				<Route index element={<Home />} errorElement={<ErrorPages />} />
 				<Route path='/skills' element={<Skills />} errorElement={<ErrorPages />} />
 				<Route path='/works' element={<Works />} errorElement={<ErrorPages />} />
+				<Route path='/contact' element={<Contact />} errorElement={<ErrorPages />} />
 			</Route>
 		</Route>,
 	),
---
+++ app/src/pages/contact/container/index.jsx
@@ -0,0 +1,40 @@
+import { FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa6';
+import { Link } from 'react-router-dom';
+
+const contacts = [
+	{
+		platform: 'Linked In',
+		text: 'I Putu Phillip Steven',
+		link: `https://www.linkedin.com/in/iputuphillipsteven/`,
+		icon: <FaLinkedin className='text-2xl' />,
+	},
+	{
+		platform: 'Github',
+		text: 'putuphillipsteven',
+		link: `https://github.com/putuphillipsteven`,
+		icon: <FaGithub className='text-2xl' />,
+	},
+	{
+		platform: 'Instagram',
+		text: 'putuphillipsteven',
+		link: `https://www.instagram.com/putuphillipsteven`,
+		icon: <FaInstagram className='text-2xl' />,
+	},
+];
+export const Contact = () => {
+	const renderedContact = contacts?.map((contact) => {
+		return (
+			<Link className='flex items-center gap-x-2' to={`${contact?.link}`} target='_blank'>
+				<li>{contact.text}</li>
+				{contact?.icon}
+			</Link>
+		);
+	});
+	return (
+		<div className='flex flex-row-reverse h-full b overflow-hidden w-full'>
+			<div className='flex flex-col self-end w-full sm:w-8/12 h-fit gap-y-2'>
+				<ul className='flex flex-col gap-y-2 self-end items-end'>{renderedContact}</ul>
+			</div>
+		</div>
+	);
+};
