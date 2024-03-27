577c4743735d00a09637e71e279b127950a5a2d4
phillip-steven <putu.phillip@gmail.com>
2024-03-27 13:59:50 +0700

    fix(works)
    adding some details on works description,
    and also add techstack for the projects

--- app/src/pages/works/container/index.jsx
+++ app/src/pages/works/container/index.jsx
@@ -4,23 +4,29 @@ import { MdArrowOutward } from 'react-icons/md';
 const works = [
 	{
 		name: 'Pure Multiwarehouse E-Commerce',
-		description: `A web app for companies like Uniqlo that has many warehouse.`,
-		link: `https://github.com/purwadhikafullstack/JCWD021001`,
+		description: `A Web App for companies like Uniqlo that has many warehouse. In this app, we can manage our users, warehouses, products, stocks, reports, and transactions.`,
+		techStacks: [`Javascript`, `My Sql`, `React`, `Express`, `Node`, `Sequelize`, `Chakra UI`],
+		link: `https://github.com/putuphillipsteven/pure/tree/development`,
 		imageLink: `https://lh3.googleusercontent.com/d/1YeFUJClIoVNvnWUAtmSLYe9dhuXYamw_=w1000?authuser=0`,
 	},
 	{
 		name: 'Circle-K POS',
-		description: `Point Of Sales web app that similar to the Circle-K design`,
+		description: `Point Of Sales Web App that similar to the Circle-K design. In this app, we can manage our products, reports, and transactions`,
+		techStacks: [`Typescript`, `My Sql`, `React`, `Express`, `Node`, `Sequelize`, `Chakra UI`],
+		link: `https://github.com/putuphillipsteven/PSS-FE.git`,
 		imageLink: `https://lh3.googleusercontent.com/d/1Ua4UMcuSx7m-Zso49I00Mg-nWRcuiEIz=w1000?authuser=0`,
 	},
 	{
 		name: 'WarungTiket Event Management',
-		description: `Event Management Web App like Loket.com but with very simple feature`,
+		description: `Event Management Web App like Loket.com but with very simple feature. This app give us feature like create an event, and buying tickets`,
+		techStacks: [`Javascript`, `My Sql`, `React`, `Express`, `Node`, `Sequelize`, `Chakra UI`],
+		link: `https://github.com/putuphillipsteven/warungtiket.git`,
 		imageLink: `https://lh3.googleusercontent.com/d/1H46bh2iJzbWWOnrG3TBOZm2mfWRvD-4t=w1000?authuser=0`,
 	},
 	{
 		name: 'Calculator',
 		description: `Simple calculator app`,
+		techStacks: [`HTML`, `CSS`, `Javascript`],
 		link: `https://putuphillipsteven.github.io/calculator/`,
 		imageLink: `https://lh3.googleusercontent.com/d/1QJCZelGz5PDW9UZM58UVEJkglZkjLUnq=w1000?authuser=0`,
 	},
@@ -28,8 +34,15 @@ const works = [
 
 export const Works = () => {
 	const renderedWorks = works?.map((work) => {
+		const renderedTechStacks = work?.techStacks?.map((tech) => {
+			return (
+				<div className=' bg-black rounded-lg text-white px-2 py-1'>
+					<p className='text-xs'>{tech}</p>
+				</div>
+			);
+		});
 		return (
-			<div className='flex flex-col gap-y-2 w-full' key={[work?.name]}>
+			<div className='flex flex-col gap-y-2 w-full justify-between' key={[work?.name]}>
 				<Link
 					to={`${work?.link}`}
 					target='_blank'
@@ -43,6 +56,7 @@ export const Works = () => {
 				<div className='w-full aspect-video border-black border-2'>
 					<img src={`${work?.imageLink}`} />
 				</div>
+				<div className='flex flex-wrap lg:grid-cols-3 gap-1'>{renderedTechStacks}</div>
 			</div>
 		);
 	});
