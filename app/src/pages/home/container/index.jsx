5f290f585fe045b95e8e947a947e4bc929cf9405
phillip-steven <putu.phillip@gmail.com>
2024-03-27 14:22:34 +0700

    fix(home)
    tidy up self introduction

--- app/src/pages/home/container/index.jsx
+++ app/src/pages/home/container/index.jsx
@@ -5,12 +5,9 @@ export const Home = () => {
 				<p className='text-justify'>
 					I am a graduate in automation engineering from State Polytechnic of Bali, graduated in
 					November 2021. Interested in pursuing and looking for opportunities in web development
-					role. Certified in front and back end development. Skilled in HTML, CSS, Javascript, Node,
-					React, and MySql. Recently took a Full Stack Development bootcamp in Purwadhika Digital
-					School. In this bootcamp, I create simple Event Ticket Management, Point of Sales
-					Application, and Multi Warehouse E-Commerce Web App, all is done in groups and using MERN
-					tech stack. Interested in finding a better problem solving for challenging tasks, and
-					learning new technologies and tools.
+					role. Certified in front end, back end, and full stack web development. Skilled in HTML,
+					CSS, JavaScript, React, Node, Express, and My SQL. Interested in finding a better problem
+					solving for challenging tasks, and learning new technologies and tools.
 				</p>
 			</div>
 		</div>
